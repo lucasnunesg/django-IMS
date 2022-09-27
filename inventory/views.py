1644aea27ab253e9f34b3d3017525fb8e7493722
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2022-09-27 00:12:52 -0300

    Added bootstrap elements

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        "title": "Index Page"
+    }
+    return render(request, "inventory/index.html", context=context)
