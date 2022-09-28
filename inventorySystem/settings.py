4327b5cc3a1a8b9952d741a034b9f1a15ca42c16
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2022-09-27 22:26:48 -0300

    Updated requirements.txt

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,8 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy forms template pack (login)
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Login Urls
+LOGIN_REDIRECT_URL = "/inventory"
+LOGIN_URL = "login"
--- inventorySystem/urls.py
+++ inventorySystem/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("inventory.urls"))
+    path('inventory/', include("inventory.urls")),
+    path("", auth_views.LoginView.as_view(template_name="inventory_system/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="inventory_system/logout.html"), name="logout")
 ]
